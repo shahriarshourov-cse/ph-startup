326980d68e4751375e8be047449ae103a70581ae
Shahriar Shourov <shahriarshourov@github.com>
2022-09-21 12:08:12 +0600

    chnage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const names = ["Shariar", "Shourov"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {names.map((name) => (
+        <li>{name}</li>
+      ))}
+      <Person name={names[1]}></Person>
+      <Person name={names[0]}></Person>
+      <Person></Person>
+      <Person></Person>
+      {/* <Info></Info> problem */}
     </div>
   );
+}
+
+function Person(props) {
+  return (
+    <div className="person">
+      <h1>
+        Inn'Shaa'Allah, I will be a top ReactJS Developer In 2022 in the world
+      </h1>
+      <p>{props.name} FullStack Developer</p>
+    </div>
+  );
+  function Info() {
+    return (
+      <div className="info">
+        <h1>I am from Info Component</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
